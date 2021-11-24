b58f277945eba38003a770730033b796be157c77
Edgar Junior Otero Rada <dgrotero697@gmail.com>
2021-11-24 04:16:29 -0500

    hashrouter and error 404

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Register from "./pages/register";
 import PrivateRoute from "./Routes/PrivateRoute";
 import RouteLogin from "./Routes/RouteLogin";
 import RouteRegister from "./Routes/RouteRegister";
+import Error404 from "./pages/error";
+import { Route } from "react-router";
 import { Switch } from "react-router";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -13,9 +15,10 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <RouteRegister path="/register" component={Register} />
-        <PrivateRoute path="/home" component={Home} />
-        <RouteLogin path="/" component={Login} />
+        <RouteRegister exact path="/register" component={Register} />
+        <PrivateRoute exact path="/home" component={Home} />
+        <RouteLogin exact path="/" component={Login} />
+        <Route path="*" component={Error404} />
       </Switch>
     </div>
   );
